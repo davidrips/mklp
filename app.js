6358f49348aa80de43f7127d73ffe598e17bfca7
David Rips <davidrips@github.com>
2016-10-07 18:34:06 -0400

    db1

--- app.js
+++ app.js
@@ -27,19 +27,19 @@ app.use(cookieParser());
 
 var Sequelize = require('sequelize');
 
-var pg = require('pg');
+// var pg = require('pg');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+// pg.defaults.ssl = true;
+// pg.connect(process.env.DATABASE_URL, function(err, client) {
+//   if (err) throw err;
+//   console.log('Connected to postgres! Getting schemas...');
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
+//   client
+//     .query('SELECT table_schema,table_name FROM information_schema.tables;')
+//     .on('row', function(row) {
+//       console.log(JSON.stringify(row));
+//     });
+// });
 
 var sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
